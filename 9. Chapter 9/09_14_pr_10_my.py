12a379e3e3026bdf9a4b15a1a7182666c55a7272
sandipsaha2005 <iamsandipsaha2005@gmail.com>
2022-08-08 17:36:15 +0530

    CHANGES

--- 9. Chapter 9/09_14_pr_10_my.py
+++ 9. Chapter 9/09_14_pr_10_my.py
@@ -1,6 +1,4 @@
-#Write a program to wipe out the contents of a file using python.
 with open("akshay.txt") as f:
-    a=f.read()
+    content=f.read()
 with open("akshay.txt","w") as f:
-    a=f.write("")
-    
+    f.write("")
---
+++ 9. Chapter 9/09_15_pr_11_my.py
@@ -0,0 +1,41 @@
+#Write a python program to rename a file to “renamed_by_python.txt.”
+# import os
+
+# oldname="akshay.txt"
+# newname="anushka.txt"
+
+# os.rename(oldname,newname)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import os
+# oldname="9. Chapter 9\\altujalaltu.txt"
+# newname="jaisriram"
+# with open(oldname) as f:
+#     content=f.read()
+# with open(newname,"w") as f:
+#     f.write(content)
+# os.remove(oldname)
+
+
+
+   
